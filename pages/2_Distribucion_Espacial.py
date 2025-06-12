5657bda956e9518b6bab661959a3ce4bcd602cc5
Daniel Córdoba <157246808+danielcba@users.noreply.github.com>
2025-06-12 01:20:19 -0300

    Update 2_Distribucion_Espacial.py

--- pages/2_Distribucion_Espacial.py
+++ pages/2_Distribucion_Espacial.py
@@ -540,7 +540,7 @@ with tab4:
                 color=location_color,  # Color único por ubicación
                 fill=True,
                 fill_color=location_color,  # Mismo color para el relleno
-                fill_opacity=1,  # 30% de opacidad para el relleno
+                fill_opacity=1.5,  # 30% de opacidad para el relleno
                 weight=0.5,  # Borde más fino para mejor apariencia
                 tooltip=None  # Sin tooltip al pasar el mouse
             )
